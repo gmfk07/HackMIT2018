2efc63a11acc39160b20caa6243c48759a0641d3
Julian Hernandez <gmfk07@gmail.com>
2018-09-15 13:08:53 -0400

    Code generation, socket-io.client

--- server.py
+++ server.py
@@ -5,11 +5,14 @@ Created on Sat Sep 15 11:26:48 2018
 @author: gmfk07
 """
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, join_room, leave_room, emit
+from words import adjective_set, noun_set
+import random
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+currentLobbies = set()
 
 @socketio.on('join', namespace='/game')
 def on_join(rm):
@@ -28,4 +31,27 @@ def test_connect():
 @socketio.on('disconnect', namespace='/server')
 def test_disconnect():
     print('Client disconnected')
+
+def post(data, code=200):
+    resp = jsonify(data)
+    resp.status_code = code
+    resp.headers['Link'] = 'http://build-it-yourself.com'
+    return resp
+
+def generate_code():
+    while True:
+        current_adj = random.sample(adjective_set, 1)[0]
+        current_noun = random.sample(noun_set, 1)[0]
+        current_code = current_adj + " " + current_noun
+        if (current_code not in currentLobbies):
+            return current_code
+        
     
+@app.route('/lobby', methods=["CONNECT"])
+def login():
+    #Adding users
+    if request.method == "CONNECT":
+        if "game" in request.args: 
+            code = generate_code()
+            currentLobbies.add(code)
+            return post({"code": code})
---
+++ words.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 15 12:47:30 2018
+
+@author: gmfk07
+"""
+
+adjective_set = {"required", "scientific", "sufficient", "advanced", 
+                  "ultimate", "unlimited", "dope", "sick", "cool", "radical",
+                  "unknown", "awesome", "sad"}
+noun_set = {"bottle", "pen", "pineapple", "apple", "chair", "president",
+              "panty", "hose", "pantyhose", "game", "time", "existence",
+              "ball"}
