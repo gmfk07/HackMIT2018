97937923440cfd5b1c70914f49cc0d6418a88c6f
Belinda Shi <belindashi@outlook.com>
2018-09-15 14:35:43 -0400

    Add controller CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Controller.css';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:5000/test', {reconnect: true});
@@ -11,9 +12,52 @@ class Controller extends React.Component {
     socket.emit('join', 'main');
   }
 
+  handleClick() {
+    alert("hi");
+  }
+
   render()
   {
-    return (null);
+    return (
+<div>
+<div class="cable"></div>
+<div class="controller">
+  <div class="centerBlue">
+    <div class="centerLeft"></div>   
+    <div class="centerRight"></div>  
+  </div>
+  <div class="centerStart">
+    <div class="SLeft"></div>
+    <div class="SRight"></div>
+  </div>
+  <div class="centerSelect">
+    <div class="SLeft"></div>
+    <div class="SRight"></div>
+  </div>
+ 
+  <div class="controllerLeft">
+    <div class="circle"></div>
+    <div class="crossCenter">
+      <div class="crossTop"></div>
+      <div class="crossBottom"></div>
+      <div class="crossLeft"></div>
+      <div class="crossRight"></div>
+      <div class="crossCircle"></div>
+    </div>
+  </div>
+  <div class="controllerRight">
+    <div class="backButton1Center">
+      <div class= "cornerLeft1" onClick={this.handleClick}></div>
+      <div class= "cornerRight1"></div>
+    </div>
+    <div class="backButton2Center">
+      <div class= "cornerLeft2"></div>
+      <div class= "cornerRight2"></div>
+    </div>
+  </div>
+</div>
+</div> 
+    );
   }
 }
 
